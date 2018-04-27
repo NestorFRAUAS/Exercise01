c50b2a9a233457e0c0904cbbc18aea3e97edc2f0
NestorFRAUAS <thrashhead7@gmail.com>
2018-04-27 22:32:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/roggenbuck/exercise01/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.roggenbuck.exercise01;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public EditText editTextRed;
+
+    public int editTxtRedNum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        editTextRed = (EditText) findViewById(R.id.editTextRed);
+
+    }
+
+    TextWatcher txtWatch = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            if(editTextRed.getText().length()>=1){
+                int number = Integer.parseInt(editTextRed.getText().toString());
+                if(number >= 0 && number <= 255){
+                    editTxtRedNum = number;
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Allowed numbers are within range 0 to 255. ", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
