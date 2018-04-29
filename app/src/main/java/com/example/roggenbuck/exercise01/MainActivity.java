0984923249529d532792485e6a4977f27a96c388
NestorFRAUAS <nroggenb@stud.fra-uas.de>
2018-04-29 16:26:41 +0200

    Need to handle empty String, because App crashes. (See Textwatcher).

--- app/src/main/java/com/example/roggenbuck/exercise01/MainActivity.java
+++ app/src/main/java/com/example/roggenbuck/exercise01/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextRed, editTextGreen, editTextBlue;
     private SeekBar seekBarRed, seekBarGreen, seekBarBlue;
 
-    private int editTextRedNum;
+    private int editTextRedNum, editTextGreenNum, editTextBlueNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +37,108 @@ public class MainActivity extends AppCompatActivity {
         seekBarGreen = (SeekBar) findViewById(R.id.seekBarGreen);
         seekBarBlue = (SeekBar) findViewById(R.id.seekBarBlue);
 
+        editTextRed.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(Integer.parseInt(editTextRed.getText().toString()) >= 0
+                        && Integer.parseInt(editTextRed.getText().toString()) < 256)
+                {
+                    editTextRedNum = Integer.parseInt(editTextRed.getText().toString());
+                    seekBarRed.setProgress(editTextRedNum);
+                }
+
+                else
+                    {
+                        seekBarRed.setProgress(editTextRedNum);
+                    }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(Integer.parseInt(editTextRed.getText().toString()) > 255){
+                    editTextRedNum = 255;
+                    editTextRed.setText(""+ editTextRedNum);
+                }
+                editTextRed.setSelection(editTextRed.length());
+            }
+        });
+
+        editTextGreen.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if(Integer.parseInt(editTextGreen.getText().toString()) >= 0
+                        && Integer.parseInt(editTextGreen.getText().toString()) < 256)
+                {
+                    editTextGreenNum = Integer.parseInt(editTextGreen.getText().toString());
+                    seekBarGreen.setProgress(editTextGreenNum);
+                }
+
+                else
+                    {
+                        editTextGreenNum = 255;
+                        seekBarGreen.setProgress(editTextGreenNum);
+
+                    }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(Integer.parseInt(editTextGreen.getText().toString()) > 255){
+                    editTextGreenNum = 255;
+                    editTextGreen.setText(""+ editTextGreenNum);
+                }
+                editTextGreen.setSelection(editTextGreen.length());
+            }
+        });
+
+        editTextBlue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(Integer.parseInt(editTextBlue.getText().toString()) >= 0
+                        && Integer.parseInt(editTextBlue.getText().toString()) < 256)
+                {
+                    editTextBlueNum = Integer.parseInt(editTextBlue.getText().toString());
+                    seekBarBlue.setProgress(editTextBlueNum);
+                }
+
+                else
+                    {
+                        editTextBlueNum = 255;
+                        seekBarBlue.setProgress(editTextBlueNum);
+                    }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(Integer.parseInt(editTextBlue.getText().toString()) > 255){
+                    editTextBlueNum = 255;
+                    editTextBlue.setText(""+ editTextBlueNum);
+                }
+                editTextBlue.setSelection(editTextBlue.length());
+            }
+        });
+
+        // Handling SeekBars
+        // If you change it, the value shall appear in the corresponding EditText View
         seekBarRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
@@ -87,6 +189,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
 
     @Override
